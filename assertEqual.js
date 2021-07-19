43c68a3a1618f266b6a3c788524725b40a9edcf4
Louis Manlangit <louism.coding@gmail.com>
2021-07-19 18:38:58 +0000

    Replaced concatnate with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+    //console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
   }
   if (actual !== expected) {
-    console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
