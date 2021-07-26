f3e5f919dbe1156e8ab8624cd445d54b442df52a
Louis Manlangit <louism.coding@gmail.com>
2021-07-26 16:59:57 +0000

    Refractor tail.js; moved test code to tailTest.js; exported tail.js and required it in tailTest.js

--- tail.js
+++ tail.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    //console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let tails = [];
   for (let x = 1; x < array.length; x++) {
@@ -16,6 +6,4 @@ const tail = function(array) {
   return tails;
 };
 
-const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
