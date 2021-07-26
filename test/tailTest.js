58d6dd53a62658b6d7154e37acca9dabfad9d459
Louis Manlangit <louism.coding@gmail.com>
2021-07-26 19:24:05 +0000

    Refractored middleTest.js and tailTest.js. Removed assert function, used mocha and chai to assert and test.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-tail(words);
-assertEqual(words.length, 3);
+describe('#tail', () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs'])
+  })
+  it("returns ['beep', 'boop', 'bop'] for ['Yo Yo', 'beep', 'boop', 'bop']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs'])
+  })
+})
